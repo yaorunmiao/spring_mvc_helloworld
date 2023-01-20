a7400150030e5ecb585e2d162951a1d0f6037894
姚润淼 <1650952708@qq.com>
2023-01-20 11:23:15 +0800

    second

---
+++ src/main/java/com/atguigu/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.atguigu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String protal() {
+//        ½«Âß¼­ÊÓÍ¼·µ»Ø
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "success";
+    }
+}
